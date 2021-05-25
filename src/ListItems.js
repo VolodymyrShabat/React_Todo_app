85965b80bcb8bdd672b8a8d666928f5e9c6c2200
volodymyr.shabat <volodymyr.shabat@intenselab.com>
2021-05-25 18:39:46 +0300

    Fixed mistake

--- src/ListItems.js
+++ src/ListItems.js
@@ -6,22 +6,10 @@ function ListItems(props){
     const items=props.items;
     const listItems = items.map(item=>
         {
-            if(item.completed!==true)
-            {
-                return <div className="list" key={item.key}>
-                    
-                        <p>{item.text}
-                        <input type="checkbox" onChange={()=>props.toggleComplete(item.key)} checked={items.completed}></input>
-                        <span>
-                            <FontAwesomeIcon className="faicons" 
-                            icon='trash' 
-                            onClick={()=>props.deleteItem(item.key)}/>
-                        </span>
-                        </p>
-                        </div>
-            }
+
             return <div className="list" key={item.key}>
-                    <p className="completed">{item.text}
+                
+                    <p className= {item.completed!==true?"":"completed"}>{item.text}
                     <input type="checkbox" onChange={()=>props.toggleComplete(item.key)} checked={items.completed}></input>
                     <span>
                         <FontAwesomeIcon className="faicons" 
